96f6b557d9e782835d93d80c190e19e63d3567c3
Sandor Zeestraten <sandor@zeestrataca.com>
2016-11-08 14:04:29 +0100

    Added clickable dropdown menu and added script SSI

---
+++ Documents/js/dropdownmenu.js
@@ -0,0 +1,28 @@
+/*
+FILENAME: dropdownmenu.js
+PURPOSE: Add a clickable dropdown menu with CSS and JS.
+CREDITS: The code is a lightly modified version from the Dropdown Menu in Navbar example from W3Schools.
+URL: http://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar_click
+*/
+
+// Toggle between hiding and showing the dropdown content when clicked
+function toggleDropdownMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  // Check if target is not dropdown button
+  if (!e.target.matches('.dropbtn')) {
+    // Go through all dropdown classes
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      // Loop through all open dropdown menus
+      var openDropdown = dropdowns[d];
+      // If any dropdowns are open, remove them
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
